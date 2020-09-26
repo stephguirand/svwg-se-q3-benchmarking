61ab6f65f4e0c7613f594caf806a73d9c8cfb17e
svw <stephguirand@aol.com>
2020-09-25 23:28:14 -0400

    Passed all tests

--- anagrams.py
+++ anagrams.py
@@ -10,7 +10,13 @@ for an arbitrary list of strings.
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = """
+stephguirand
+Help from demo, lessons and activities, youtube videos in canvas and
+own search on youtube,
+stack overflow, Tutors, Facilitators and talking about assignment
+in study group.
+"""
 
 import sys
 
@@ -27,11 +33,20 @@ def find_anagrams(words):
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
+
+    # anagrams = {
+    #     alphabetize(word): [
+    #         w for w in words
+    #         if alphabetize(w) == alphabetize(word)]
+    #     for word in words}
+
+    anagrams = {}
+    for word in words:
+        # Insert a key,
+        # --set a default value
+        anagrams.setdefault(alphabetize(word), [])
+        if word not in anagrams[alphabetize(word)]:
+            anagrams[alphabetize(word)].append(word)
     return anagrams
 
 
